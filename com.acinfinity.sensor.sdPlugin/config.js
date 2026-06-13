efa88caa7431f2f8334095aa4fe4141830e9e415
Jonax <48924461+JonaxScript@users.noreply.github.com>
2026-06-13 23:49:27 +0200

    Update config.js

--- com.acinfinity.sensor.sdPlugin/config.js
+++ com.acinfinity.sensor.sdPlugin/config.js
@@ -1,8 +1,8 @@
 // AC Infinity Zugangsdaten – diese Datei mit einem Texteditor bearbeiten
 // Speichern und dann Stream Deck neu starten
 window.AC_INFINITY_CONFIG = {
-    email:           "deine@email.de",
-    password:        "DeinPasswort",
+    email:           "your@email.de",
+    password:        "password",
     deviceIndex:     0,
     tempUnit:        "C",
     refreshInterval: 30
